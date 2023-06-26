8e6079cc064ff66ea3fb39af8072528ce5d15f98
Fulvio Cusimano <fulvio.cusimano@code.seat>
2023-06-26 15:42:06 +0200

    chore: wip

--- scripts/.npmrc-gen.sh
+++ scripts/.npmrc-gen.sh
@@ -3,5 +3,5 @@ PACKAGES_MODULES=(`ls packages | grep -v 'node_modules'`)
 for i in "${PACKAGES_MODULES[@]}"
   do
     : 
-    echo -e "@fuvizzo:registry=https://npm.pkg.github.com \n//npm.pkg.github.com/:_authToken= $1 \nalways-auth=true"> packages/${i}/.npmrc
+    echo -e "@fuvizzo:registry=https://npm.pkg.github.com \n//npm.pkg.github.com/:_authToken= $1 \nalways-auth=true"> $2/packages/${i}/.npmrc
   done
